e8b22916c606ced71097d17e104dcbfa30ce41be
Ulan Degenbaev <udegenbaev@gmail.com>
2014-05-11 21:41:08 +0200

    Use priority queue in dijkstra.cc.

--- dijkstra.cc
+++ dijkstra.cc
@@ -1,37 +1,62 @@
 #include <vector>
 #include <cstdio>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 const int infinity = 100000000;
 const int N = 1000;
-vector<pair<int,int> > adj[N];
-int dist[N];
-bool visited[N];
+typedef int Vertex;
+typedef int Weight;
+typedef int Distance;
+typedef set<pair<Distance, Vertex> > PriorityQueue;
 
-int main() {
-  int n, m;
+vector<pair<Vertex,Weight> > adj[N];
+
+int n, m;
+
+void read_graph() {
   scanf("%d %d", &n, &m);
   for (int i = 0; i < m; i++) {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     adj[a - 1].push_back(make_pair(b - 1, c));
   }
-  for (int i = 0; i < n; i++) dist[i] = infinity;
-  for (int i = 0; i < n; i++) visited[i] = false;
-  dist[0] = 0;
-  while (true) {
-    int v = -1;
-    for (int i = 0; i < n; i++) {
-      if (!visited[i] && (v == -1 || dist[i] < dist[v])) v = i;
-    }
-    if (v == -1) break;
-    visited[v] = true;
+}
+
+pair<Distance, Vertex> pop_min(PriorityQueue* pq) {
+  PriorityQueue::iterator it = pq->begin();
+  pair<Distance, Vertex> result = *it;
+  pq->erase(it);
+  return result;
+}
+
+Distance dist[N];
+void adjust(PriorityQueue* pq, Vertex k, Distance d) {
+  pq->erase(make_pair(dist[k], k));
+  pq->insert(make_pair(d, k));
+  dist[k] = d;
+}
+
+
+int main() {
+  read_graph();
+  PriorityQueue pq;
+  for (int i = 0; i < n; i++) {
+    dist[i] = infinity;
+    pq.insert(make_pair(infinity, i));
+  }
+  adjust(&pq, 0, 0);
+  while (!pq.empty()) {
+    pair<Distance, Vertex> dv = pop_min(&pq);
+    Vertex v = dv.second;
     for (size_t i = 0; i < adj[v].size(); i++) {
-       int nv = adj[v][i].first;
-       int nw = adj[v][i].second;
-       dist[nv] = min(dist[nv], dist[v] + nw);
+       Vertex nv = adj[v][i].first;
+       Weight nw = adj[v][i].second;
+       if (dist[nv] > dist[v] + nw) {
+          adjust(&pq, nv, dist[v] + nw);
+       }
     }
   }
   printf("%d\n", dist[n-1]);
